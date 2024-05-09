5fc3096f55c002ebef64cf192682474abe45ede0
the-real-ak <i.write.code.and.lift.weight@gmail.com>
2024-05-09 13:10:54 -0400

    Updating exit with list comprehension

--- day-25-us-states-game-start/main.py
+++ day-25-us-states-game-start/main.py
@@ -19,15 +19,15 @@ class StateWriter(Turtle):
         self.penup()
         self.data = data
 
-    def write_state(self, state):
-        row = self.data[self.data.state == state]
+    def write_state(self, state_item):
+        row = self.data[self.data.state == state_item]
         x = int(row.x)
         y = int(row.y)
         self.goto(x, y)
-        self.write(state, align="center", font=("Ariel", 10, "normal"))
+        self.write(state_item, align="center", font=("Ariel", 10, "normal"))
 
     def win(self):
-        self.goto(0,0)
+        self.goto(0, 0)
         self.write("YOU WIN!", align="center", font=("Ariel", 10, "normal"))
 
 
@@ -38,21 +38,11 @@ while len(guessed_states) != 50:
     answer_state = screen.textinput(f"Guess the state. {len(guessed_states)}/50",
                                     prompt="What's another states name?").title()
     if answer_state == "Exit":
+        data_list = [state for state in states.to_list() if state not in guessed_states]
+        pandas.DataFrame(data_list).to_csv("missing_states.csv")
         break
     if answer_state in states.to_list():
         guessed_states.append(answer_state)
         jenny.write_state(answer_state)
-
-data_list = []
-
-for state in states.to_list():
-    if state not in guessed_states:
-        data_list.append(state)
-
-print(data_list)
-pandas.DataFrame(data_list).to_csv("missing_states.csv")
-
-
-
 
 screen.exitonclick()
